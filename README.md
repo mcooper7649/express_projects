da0745a443bd12be0d8f6346ff813660713ceb5c
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2020-03-26 13:22:15 -0400

    added weather app api

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # express_projects
+
+
+Welcome to my repo for small node / express projects. Feel free to fork. I will try to add consistenly.
---
+++ weather-project/app.js
@@ -0,0 +1,55 @@
+const https = require("https");
+const express = require("express");   //Default express confiration needed
+const bodyParser = require("body-parser")
+const app = express();
+
+
+
+
+app.use(bodyParser.urlencoded({extend: true}));
+
+app.get("/", function (req, res){
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.post("/", function (req, res) {
+    const query = req.body.cityName
+    const apiKey = "34c9ed530825ec44ee8170d08491a54e" 
+    const unit = "imperial"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit
+    https.get(url, function(response) {
+        console.log(response.statusCode);
+
+        response.on("data", function(data){
+            const weatherData =  JSON.parse(data)
+            const temp = weatherData.main.temp
+            const icon = weatherData.weather[0].icon
+            const imageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+            const weatherDescription = weatherData.weather[0].description
+            console.log(weatherDescription)
+            res.write("<p>Weather Description: " + weatherDescription + "</p>")
+            res.write("<h1>The temperature in " + query + " is " + temp + " degrees.</h1>")
+            res.write("<img src=" + imageUrl + "></img>")
+            res.send()
+        })
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function(){
+    console.log("server is running on port 3000.")
+})
